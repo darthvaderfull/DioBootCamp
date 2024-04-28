4a032eeb88c5dad36e4a5e591018c21f5e3ef540
Eliel Nascimento <elielsantosdonascimento@gail.com>
2024-04-28 19:09:47 -0300

    Teste medias de alunos

---
+++ boletimEstudantil.java
@@ -0,0 +1,12 @@
+//identação
+public class boletimEstudantil {
+
+	public static void main(String[] args){
+		int mediaFinal = 6;
+			if(mediaFinal<6) {
+				System.out.println("Aluno Reprovado");
+				}
+			else 
+		System.out.println("Aluno Aprovado");	
+	}
+ }
